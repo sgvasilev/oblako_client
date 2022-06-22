f3b708fe9f8f37507c33119ab6596368c13e8048
Sergey <sendto.vasilevsg@gmail.com>
2022-06-22 10:01:08 +0300

    fixed env

--- src/app/project-api.service.ts
+++ src/app/project-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Project as ProjectModel } from './model/project.model';
 
 export interface Project {
@@ -19,7 +20,7 @@ export interface Todo {
   providedIn: 'root',
 })
 export class ProjectApiService {
-  private projectUrl = 'http://localhost:3000/projects';
+  private projectUrl = environment.URL;
   constructor(private http: HttpClient) {}
   public changeStatus(
     projectId: number,
